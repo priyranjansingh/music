2d3856aae39f81ed07cfb63391c38e5493c91d43
priyranjansingh <singh.priyranjan@gmail.com>
2016-03-09 08:28:31 +0530

    home page work

--- protected/modules/home/controllers/DefaultController.php
+++ protected/modules/home/controllers/DefaultController.php
@@ -1,9 +1,15 @@
 <?php
 
-class DefaultController extends Controller
-{
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
+class DefaultController extends Controller {
+
+    public function actionIndex() {
+
+        $albums = BaseModel::getAll('Albums', array(
+                    "order" => "date_entered DESC",
+                    "limit" => 20,
+        ));
+        //pre($albums,true);
+        $this->render('index',array('albums'=>$albums));
+    }
+
 }
